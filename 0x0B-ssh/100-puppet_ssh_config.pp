7fabc6797fbc87e2387d12aae0cf51d418905c9b
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-08-29 04:03:31 -0700

    ez peazy

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# make changes to config
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
+
